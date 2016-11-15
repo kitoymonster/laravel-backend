1067daf98a25510ef4cce531ff7c30703868317f
Keith Monsanto <kitoymons@gmail.com>
2016-11-15 15:30:37 +0800

    test

---
+++ database/migrations/2016_11_15_151105_create_entry_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEntryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('at_entry', function (Blueprint $table) {
+            $table->increments('id');
+			$table->string('maintenance_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('at_entry');
+    }
+}
